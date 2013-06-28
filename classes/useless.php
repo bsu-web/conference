35286a71244b83f617f74ac2e74482fd360c4d5d
nekjine <nekjine@gmail.com>
2013-06-28 22:32:35 +0900

    test commit 4

---
+++ classes/useless.php
@@ -0,0 +1,43 @@
+<?php
+/**
+    Useless demo class
+    @author nekjine
+    @version 0.0
+    @see http://example.com Documentation
+
+    @api
+*/
+
+class Useless {
+    /**
+        @type int
+    */
+    protected $trickyCounter;
+    
+    /**
+        This function does nothing
+        @param mixed magicParam Useless magic parameter
+        @return int Just a zero as a result
+    */
+    public function doNothing($magicParam){
+        echo "doNothing() call with param $magicParam";
+        return 0;
+    }
+
+    /**
+        This function returns hello world string
+        @deprecated 0.1 This method is bad and will be removed soon
+        @return string Hello world
+    */
+    public function badMethod(){
+        return "hello world";
+    }
+
+    /**
+        
+    */
+    public function __construct(){
+
+    }
+}
+?>
