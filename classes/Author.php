6f27102b1a1c0852bf8de66423111322c6b80b67
svetlanarei <rin.mei.555@gmail.com>
2013-07-06 19:06:41 +0900

    Класс Автор содержит в себе ФИО. (с комментариями)

--- classes/Author.php
+++ classes/Author.php
@@ -1,26 +1,27 @@
 <?php
 require_once('DomainObject.php');
+//класс Автор содержит в себе-Имя,Фамилию,Отчество.
 class Author extends DomainObject{
 private $name       =  "Имя автора ";
 private $family     =  "Фамилия автора ";
 private $patronymic =  "Отчество автора ";
 
-	function setName($name){
+	function setName($name){ 	 //задать Имя автора
 		$this->name = $name;
 	}
-	function getName(){
+	function getName(){ 		 //получить Имя автора
 		return $this->name;
 	}
-	function setFamily($family){
+	function setFamily($family){ //задать Фамилию автора
 		$this->family = $family;
 	}	
-	function getFamily(){
+	function getFamily(){        //получить Фамилию автора
 		return $this->family;
 	}
-	function setPatronymic($patronymic){
-		$this->patronymic = $patronymic;
+	function setPatronymic($patronymic){	//задать Отчество автора
+		$this->patronymic = $patronymic;	
 	}	
-	function getPatronymic(){
+	function getPatronymic(){				//получить Отчество автора
 		return $this->patronymic;
 	}		
 /*	function __construct($name, $family, $patronymic){
