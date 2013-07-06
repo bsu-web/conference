5917726fd68232b76c4bbe61d81af9ba77ab1f7d
Simyla <simonovaiula@gmail.com>
2013-07-06 19:01:23 +0900

    Классы Document, Paper с комментариями

--- classes/Paper.php
+++ classes/Paper.php
@@ -1,9 +1,9 @@
 <?php
 require_once('Document.php');
-
+//класс для статьи
 class Paper extends Document{
     
-    function getString(){
+    function getString(){ //получаем строку, содержащую название статьи и список авторов
         return $this->getTitle()." ".$this->getAuthors()->getString()."<br/>";
     }
     
