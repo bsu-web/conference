a86d99c14e279dca623e12d303823280838292ed
Stiller13 <dimz13@rambler.ru>
2013-07-06 23:43:54 +0900

    Полезное хранилище для данных,которые нужно передать уровню представления

---
+++ classes/Request.php
@@ -0,0 +1,40 @@
+<?php
+require_once('../classes/RequestRegistry.php');
+class Request{
+	private $properties;
+	private $feedback=array();
+	function __construct(){
+		$this->init();
+		RequestRegistry::setRequest($this);
+	}
+	function init(){
+		if (isset($_SERVER['REQUEST_METHOD'])){
+			$this->properties=$_REQUEST;
+			return;
+		}
+		foreach ($_SERVER['argv'] as $arg){
+			if (strpos($arg,'=')){
+				list($key,$val)=explode('=');
+				$this->setProperty($key,$val);
+			}
+		}
+	}
+	function getProperty($key){
+		if (isset($this->properties[$key])){
+			return $this->properties[$key];
+		}
+	}
+	function setProperty($key,$val){
+		$this->propertiesp[$key]=$val;
+	}
+	function addFeedback($msg){
+		array_push($this->feedback,$msg);
+	}
+	function getFeedback(){
+		return $this->feedback;
+	}
+	function getFeedbackString($separator="\n"){
+		return implode($separator,$this->feedback);
+	}
+}
+?>
