bf2919261b08e664f466aa9908067a2cb4f50e99
j1o1 <locust_king@mail.ru>
2013-07-20 16:23:12 +0900

    +функции вывода статей для журнала

--- lessons/test_Command/Document.php
+++ lessons/test_Command/Document.php
@@ -1,9 +1,9 @@
 <?php
 require_once('DomainObject.php');
 /**
- * абстрактный класс- документ (статья или тезис или презентация)
+ * абстрактный класс - документ (статья или тезис или презентация)
  * @author Симонова Ю.
- * @todo Будет апгрейдится)
+ * @todo Будет апаться)
  * @package files
  */
 abstract class Document extends DomainObject{
@@ -19,6 +19,9 @@ abstract class Document extends DomainObject{
     function setAuthors(AuthorCollection $authors){
         $this->authors= $authors;        
     }
+	function setPapers(PaperCollection $papers){
+        $this->papers= $papers;        
+    }
     /**
      * выдаем список авторов
      */    
@@ -28,13 +31,27 @@ abstract class Document extends DomainObject{
         }
         return $this->authors;
     }
+    function getPapers(){   
+        if (! isset($this->papers)){
+            $this->papers= self::getCollection('Paper');
+        }
+        return $this->papers;
+    }
+	
     
     function getAuthorStr(){
         foreach ($this->authors as $author){
             echo $author->getFamily().'  '.$author->getName().'  '.$author->getPatronymic().'</br>';    
         }
     }
-    
+    function getPaperStr(){
+        foreach ($this->papers as $paper){
+            echo $author->getTitle().'  '.$author->getContent().'</br>'; 
+			echo "написать как выводить авторов статей в журнале<br>"
+			//--^
+        }
+    }
+	
     function addAuthor(Author $author){
         $this->getAuthors()->add($author);
     }
